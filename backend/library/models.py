298f75d4889758d16907bc6bc0092b523ef52b05
Dante <github.6p8ng@kironmail.com>
2026-03-15 15:33:21 -0600

    chore: improve the models and make migrations

--- backend/library/models.py
+++ backend/library/models.py
@@ -3,12 +3,32 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
+    bio = models.TextField(blank=True, null=True)
+
+
+class BookSource(models.TextChoices):
+    EPUB = "epub", "Epub"
+    WISHLIST = "wishlist", "Wishlist"
 
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
     authors = models.ManyToManyField(Author)
     description = models.TextField(blank=True)
-    language = models.CharField(max_length=10, blank=True)
-    file_path = models.TextField()
+    file = models.TextField(blank=True, null=True)
+
+    cover_url = models.TextField(blank=True, null=True)
+    asin = models.CharField(max_length=20, blank=True, null=True)
+    isbn = models.CharField(max_length=20, blank=True, null=True)
+    isbn13 = models.CharField(max_length=20, blank=True, null=True)
+    pages = models.IntegerField(blank=True, null=True)
+    publication_time = models.DateTimeField(blank=True, null=True)
+    publisher = models.CharField(max_length=255, blank=True, null=True)
+    language = models.CharField(max_length=50, blank=True, null=True)
+
+    currently_reading = models.BooleanField(default=False)
+    read = models.BooleanField(default=False)
+
+    type = models.CharField(max_length=10, choices=BookSource.choices, default="epub")
+
     created_at = models.DateTimeField(auto_now_add=True)
--- backend/metadata/models.py
+++ backend/metadata/models.py
@@ -2,11 +2,14 @@ from library.models import Book
 from django.db import models
 
 
-class MetadataSource(models.Model):
-    name = models.CharField(max_length=50)
+class MetadataSource(models.TextChoices):
+    GOODREADS = "goodreads", "Goodreads"
 
 
 class ExternalBookID(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    source = models.ForeignKey(MetadataSource, on_delete=models.CASCADE)
+    source = models.CharField(
+        max_length=20,
+        choices=MetadataSource.choices,
+    )
     external_id = models.CharField(max_length=200)
