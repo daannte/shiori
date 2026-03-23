89e5fab30b5e27204b893697010898a7048eead8
Dante <github.6p8ng@kironmail.com>
2026-03-23 17:23:21 -0600

    feat: workspace and openapi client api

---
+++ packages/shiori-api-client/index.ts
@@ -0,0 +1,8 @@
+import createOpenAPIClient, { type ClientOptions } from 'openapi-fetch';
+
+import type { components, operations, paths } from './schema';
+export type { components, operations, paths };
+
+export function createClient(options?: ClientOptions) {
+  return createOpenAPIClient<paths>(options)
+}
