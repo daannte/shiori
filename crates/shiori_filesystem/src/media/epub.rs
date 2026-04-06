e786a9f778c8a84afe1b96837a6c2e1337c9817b
Dante <85536418+daannte@users.noreply.github.com>
2026-04-06 12:05:46 -0600

    refactor(cover): rename cover to `media_id` (#50)
    * refactor: ended up changing the cover name to the media id
    
    * chore: clippy

--- crates/shiori_filesystem/src/media/epub.rs
+++ crates/shiori_filesystem/src/media/epub.rs
@@ -6,14 +6,14 @@ pub struct Epub;
 
 impl Epub {
     // TODO: Change to return Result
-    pub fn get_cover_path(file_stem: &str, path: &Path, base_path: &Path) -> Option<String> {
+    pub fn get_cover_path(media_id: &i32, path: &Path, base_path: &Path) -> Option<String> {
         let covers_dir = base_path.join("covers");
         let epub = Rpub::open(path).ok()?;
         let cover = epub.manifest().cover_image()?;
         let ext = cover.href().extension()?;
         let data = cover.read_bytes().ok()?;
 
-        let cover_path = Path::new(&covers_dir).join(format!("{}.{}", file_stem, ext));
+        let cover_path = Path::new(&covers_dir).join(format!("{media_id}.{ext}"));
 
         fs::write(&cover_path, data).ok()?;
 
