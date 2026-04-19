56eb884ef0eb1e409028d22f3a6e80dcbef438b2
Dante <github.6p8ng@kironmail.com>
2026-04-18 22:06:40 -0600

    chore: format

--- packages/shiori-components/src/lib/index.ts
+++ packages/shiori-components/src/lib/index.ts
@@ -1,19 +1,19 @@
-export * from "./button/index.js"
-export * from "./input/index.js"
-export * from "./skeleton/index.js"
-export * from "./separator/index.js"
-export * from "./calendar/index.js"
-export * from "./label/index.js"
-export * from "./progress/index.js"
-export * from "./data-table/index.js"
+export * from './button/index.js';
+export * from './input/index.js';
+export * from './skeleton/index.js';
+export * from './separator/index.js';
+export * from './calendar/index.js';
+export * from './label/index.js';
+export * from './progress/index.js';
+export * from './data-table/index.js';
 
-export * as Accordion from "./accordion/index.js"
-export * as Collapsible from "./collapsible/index.js"
-export * as Dialog from "./dialog/index.js"
-export * as DropdownMenu from "./dropdown-menu/index.js"
-export * as Empty from "./empty/index.js"
-export * as Popover from "./popover/index.js"
-export * as Sheet from "./sheet/index.js"
-export * as Sidebar from "./sidebar/index.js"
-export * as Table from "./table/index.js"
-export * as Tooltip from "./sidebar/index.js"
+export * as Accordion from './accordion/index.js';
+export * as Collapsible from './collapsible/index.js';
+export * as Dialog from './dialog/index.js';
+export * as DropdownMenu from './dropdown-menu/index.js';
+export * as Empty from './empty/index.js';
+export * as Popover from './popover/index.js';
+export * as Sheet from './sheet/index.js';
+export * as Sidebar from './sidebar/index.js';
+export * as Table from './table/index.js';
+export * as Tooltip from './sidebar/index.js';
